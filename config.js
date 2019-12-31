9757ce714dd9f4713711655c845958cfea9e0834
Angel Salazar <angel_salazar2017@hotmail.com>
2019-12-31 17:56:33 -0600

    conexion a la db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost/db-sucursales',
+    db: process.env.MONGODB || 'mongodb+srv://angelSalazar:Monoku_6@cluster0-wxx9s.mongodb.net/test?retryWrites=true&w=majority',
     SECRET_TOKEN: '23DPR0669H'
 };
