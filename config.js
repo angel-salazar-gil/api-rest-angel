6724fcae395ce0872025faae1306004ee55cf17a
Angel Salazar <angel_salazar2017@hotmail.com>
2020-01-02 12:26:49 -0600

    cambio db

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || '3000';
 
 module.exports = {
     port: (host, port),
-    db: process.env.MONGODB || process.env.DB_URL,
+    db: 'mongodb+srv://angelSalazar:Monoku_6@cluster0-toxjc.mongodb.net/api-database?retryWrites=true&w=majority',
+    //db: process.env.MONGODB || process.env.DB_URL,
     SECRET_TOKEN: '23DPR0669H'
 };
