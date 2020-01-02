b2f91b7563298cb4ee284690ea6d57624cd2b5f0
Angel Salazar <angel_salazar2017@hotmail.com>
2020-01-02 13:04:28 -0600

    update

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || '3000';
 
 module.exports = {
     port: (host, port),
-    db: 'mongodb+srv://angelSalazar:Monoku_6@cluster0-toxjc.mongodb.net/api-database?retryWrites=true&w=majority',
-    //db: process.env.MONGODB || process.env.DB_URL,
+    db: process.env.MONGODB || process.env.DB_URL,
     SECRET_TOKEN: '23DPR0669H'
 };
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log(config.db);
 //////////////////////////////////////////////////////////////////////
 
 mongoose.set('useCreateIndex', true);
-
+/*
 mongoose.connect('mongodb+srv://angelSalazar:Monoku_6@cluster0-toxjc.mongodb.net/api-database?retryWrites=true&w=majority', {
     useUnifiedTopology: true,
     useNewUrlParser: true
@@ -23,9 +23,9 @@ console.log('Conectado a la BD ...');
 app.listen( config.port, () => {
     console.log(`API REST Corriendo en el puerto ${config.port}`);
 });
+*/
 
-/*
-mongoose.connect('mongodb+srv://angelSalazar:Monoku_6@cluster0-toxjc.mongodb.net/test?retryWrites=true&w=majority', (err, res) => {
+mongoose.connect(config.db, (err, res) => {
     useUnifiedTopology: true
     useNewUrlParser: true
     if(err) {
@@ -37,4 +37,3 @@ mongoose.connect('mongodb+srv://angelSalazar:Monoku_6@cluster0-toxjc.mongodb.net
         console.log(`API REST Corriendo en el puerto ${config.port}`);
     });
 });
-*/
